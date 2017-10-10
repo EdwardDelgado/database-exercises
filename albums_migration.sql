2d739ca1ba034ea317fed97207ba052c798859cd
Edward Delgado Jr <edward.delgado@icloud.com>
2017-10-10 12:04:42 -0500

    completed tables exercise with minor updates.

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,9 +3,9 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
-    id            INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist        VARCHAR(50),
-    name          VARCHAR(50),
+    id            INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist        VARCHAR(200),
+    name          VARCHAR(300),
     release_date  DATE,
     sales         DECIMAL(10,2) UNSIGNED NOT NULL,
     genre         VARCHAR(50),
