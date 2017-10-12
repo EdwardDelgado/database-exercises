0caef0d85eade8f55a27e2fac61174c321e67f63
Edward Delgado Jr <edward.delgado@icloud.com>
2017-10-12 10:45:20 -0500

    Completed DB Where Exercise with instructor solution.

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,18 +1,32 @@
 USE employees;
 
-SELECT first_name
-FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+SELECT
+  first_name
+  last_name
+FROM
+  employees
+WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name ='Maya'
+AND gender = 'm';
 
-SELECT last_name
-FROM employees
+SELECT
+  last_name
+FROM
+  employees
 WHERE last_name LIKE 'e%';
 
-SELECT first_name, last_name
+SELECT
+  hire_date,
+  first_name,
+  last_name
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+--   Alternate  LIKE '199%'
 
-SELECT * FROM employees WHERE birth_date LIKE '%-12-25';
+SELECT
+  birth_date,
+  first_name
+FROM employees
+WHERE birth_date LIKE '%-12-25';
 
 SELECT last_name
 FROM employees
