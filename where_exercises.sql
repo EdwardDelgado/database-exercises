77d1f6051b5c73a578c475463aee715bc7566be4
Edward Delgado Jr <edward.delgado@icloud.com>
2017-10-12 11:02:08 -0500

    Completed DB Where Continued Exercise.

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,25 +1,29 @@
 USE employees;
 
 SELECT
-  first_name
+  gender,
+  first_name,
   last_name
 FROM
   employees
-WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name ='Maya'
-AND gender = 'm';
+WHERE (first_name = 'Irena'
+      OR first_name = 'Vidya'
+      OR first_name ='Maya')
+        AND gender = 'm';
 
 SELECT
   last_name
 FROM
   employees
-WHERE last_name LIKE 'e%';
+WHERE last_name LIKE 'e%' or last_name like '%e';
 
 SELECT
-  hire_date,
+  birth_date,
   first_name,
   last_name
 FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%-12-25';
 --   Alternate  LIKE '199%'
 
 SELECT
@@ -30,4 +34,5 @@ WHERE birth_date LIKE '%-12-25';
 
 SELECT last_name
 FROM employees
-WHERE last_name LIKE '%q%';
+WHERE last_name LIKE '%q%'
+      AND last_name NOT LIKE '%qu%';
